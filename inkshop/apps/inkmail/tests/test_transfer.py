1ca8c9bc365709fb7718dfc62b3716507b2f3a49
Steven Skoczen <steven@quantumimagery.com>
2019-05-01 16:05:38 -0500

    Fixes ajax tests

--- inkshop/apps/inkmail/tests/test_transfer.py
+++ inkshop/apps/inkmail/tests/test_transfer.py
@@ -63,6 +63,7 @@ class TestPostSubscribes(MockRequestsTestCase):
         self.assertEquals(Subscription.objects.count(), 1)
         s = Subscription.objects.all()[0]
         self.assertEquals(s.person, p)
+        self.assertEquals(s.subscription_url, "transfer-subscription")
         self.assertEquals(s.newsletter.name, self.newsletter.name)
         self.assertEquals(s.subscribed_from_ip, self._source_ip)
 
@@ -87,6 +88,7 @@ class TestPostSubscribes(MockRequestsTestCase):
         self.assertEquals(Subscription.objects.count(), 1)
         s = Subscription.objects.all()[0]
         self.assertEquals(s.person, p)
+        self.assertEquals(s.subscription_url, "transfer-subscription")
         self.assertEquals(s.newsletter.name, self.newsletter.name)
         self.assertEquals(s.subscribed_from_ip, self._source_ip)
 
@@ -143,179 +145,6 @@ class TestPostSubscribes(MockRequestsTestCase):
 
         self.assertEquals(Person.objects.count(), 0)
         self.assertEquals(Subscription.objects.count(), 0)
-
-
-class TestAjaxSubscribes(MockRequestsTestCase):
-    def setUp(self, *args, **kwargs):
-        self.newsletter = Factory.newsletter()
-        super(TestAjaxSubscribes, self).setUp(*args, **kwargs)
-
-    def test_get_transfer_subscribe_200(self):
-        email = Factory.rand_email()
-        name = Factory.rand_name()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            json.dumps({
-                'f': name,
-                'e': email,
-                # 'newsletter': self.newsletter.internal_name,
-            }),
-            'json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-        )
-
-        json_string = response.content.decode('utf-8')
-        response_data = json.loads(json_string)
-        self.assertEquals(response_data["success"], True)
-        self.assertEquals(response.status_code, 200)
-
-    def test_get_transfer_subscribe_adds_person_and_subscription(self):
-        email = Factory.rand_email()
-        name = Factory.rand_name()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            json.dumps({
-                'f': name,
-                'e': email,
-                # 'newsletter': self.newsletter.internal_name,
-            }),
-            'json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-        )
-
-        json_string = response.content.decode('utf-8')
-        response_data = json.loads(json_string)
-        self.assertEquals(response_data["success"], True)
-        self.assertEquals(response.status_code, 200)
-
-        self.assertEquals(Person.objects.count(), 1)
-        p = Person.objects.all()[0]
-        self.assertEquals(p.first_name, name)
-        self.assertEquals(p.email, email)
-
-        self.assertEquals(Subscription.objects.count(), 1)
-        s = Subscription.objects.all()[0]
-        self.assertEquals(s.person, p)
-        self.assertEquals(s.newsletter.name, self.newsletter.name)
-        self.assertEquals(s.subscribed_from_ip, self._source_ip)
-
-    def test_no_first_name_subscribe_adds_person_and_subscription(self):
-        email = Factory.rand_email()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            json.dumps({
-                'e': email,
-                # 'newsletter': self.newsletter.internal_name,
-            }),
-            'json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-        )
-
-        json_string = response.content.decode('utf-8')
-        response_data = json.loads(json_string)
-        self.assertEquals(response_data["success"], True)
-        self.assertEquals(response.status_code, 200)
-
-        self.assertEquals(Person.objects.count(), 1)
-        p = Person.objects.all()[0]
-        self.assertEquals(p.email, email)
-
-        self.assertEquals(Subscription.objects.count(), 1)
-        s = Subscription.objects.all()[0]
-        self.assertEquals(s.person, p)
-        self.assertEquals(s.newsletter.name, self.newsletter.name)
-        self.assertEquals(s.subscribed_from_ip, self._source_ip)
-
-    def test_newsletter_required(self):
-        email = Factory.rand_email()
-        name = Factory.rand_name()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            json.dumps({
-                'f': name,
-                'e': email,
-            }),
-            'json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-        )
-
-        json_string = response.content.decode('utf-8')
-        response_data = json.loads(json_string)
-        self.assertEquals(response_data["success"], False)
-        self.assertEquals(response.status_code, 422)
-
-        self.assertEquals(Person.objects.count(), 0)
-        self.assertEquals(Subscription.objects.count(), 0)
-
-    def test_email_required(self):
-        name = Factory.rand_name()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            json.dumps({
-                'f': name,
-                # 'e': email,
-                # 'newsletter': self.newsletter.internal_name,
-            }),
-            'json',
-            HTTP_X_REQUESTED_WITH='XMLHttpRequest',
-        )
-
-        json_string = response.content.decode('utf-8')
-        response_data = json.loads(json_string)
-        self.assertEquals(response_data["success"], False)
-        self.assertEquals(response.status_code, 422)
-
-        self.assertEquals(Person.objects.count(), 0)
-        self.assertEquals(Subscription.objects.count(), 0)
-
-
-class TestConfirmEmail(MockRequestsTestCase):
-
-    def setUp(self, *args, **kwargs):
-        self.newsletter = Factory.newsletter()
-        super(TestConfirmEmail, self).setUp(*args, **kwargs)
-
-    def test_get_transfer_subscribe_200(self):
-        email = Factory.rand_email()
-        name = Factory.rand_name()
-        response = self.get(
-            reverse(
-                'inkmail:transfer_subscription', args=(self.newsletter.hashid,),
-            ),
-            {
-                'f': name,
-                'e': email,
-                # 'newsletter': self.newsletter.internal_name,
-            },
-        )
-        self.assertEquals(response.status_code, 200)
-        process_outgoing_message_queue()
-
-        self.assertEquals(len(mail.outbox), 1)
-        self.assertEquals(mail.outbox[0].subject, self.newsletter.confirm_message.subject)
-        self.assertEquals(OutgoingMessage.objects.count(), 1)
-        om = OutgoingMessage.objects.all()[0]
-        self.assertIn(
-            om.render_email_string(self.newsletter.confirm_message.body_text_unrendered),
-            mail.outbox[0].alternatives[0][0]
-        )
-        self.assertIn(
-            om.render_email_string(self.newsletter.confirm_message.body_text_unrendered, plain_text=True),
-            mail.outbox[0].body
-        )
-        self.assertEquals(len(mail.outbox[0].to), 1)
-        self.assertEquals(mail.outbox[0].to[0], email)
-        self.assertEquals(mail.outbox[0].from_email, self.newsletter.full_from_email)
 
 
 class TestWelcomeEmail(MockRequestsTestCase):
@@ -340,12 +169,8 @@ class TestWelcomeEmail(MockRequestsTestCase):
         self.assertEquals(response.status_code, 200)
         process_outgoing_message_queue()
 
-        s = Subscription.objects.all()[0]
-        self.get(s.opt_in_link)
-        process_outgoing_message_queue()
-
-        self.assertEquals(len(mail.outbox), 2)
-        self.assertEquals(mail.outbox[1].subject, self.newsletter.welcome_message.subject)
+        self.assertEquals(len(mail.outbox), 1)
+        self.assertEquals(mail.outbox[0].subject, self.newsletter.welcome_message.subject)
         om = OutgoingMessage.objects.all()[0]
         self.assertIn(
             om.render_email_string(self.newsletter.welcome_message.body_text_unrendered),
@@ -355,9 +180,9 @@ class TestWelcomeEmail(MockRequestsTestCase):
             om.render_email_string(self.newsletter.welcome_message.body_text_unrendered, plain_text=True),
             mail.outbox[1].body
         )
-        self.assertEquals(len(mail.outbox[1].to), 1)
-        self.assertEquals(mail.outbox[1].to[0], email)
-        self.assertEquals(mail.outbox[1].from_email, self.newsletter.full_from_email)
+        self.assertEquals(len(mail.outbox[0].to), 1)
+        self.assertEquals(mail.outbox[0].to[0], email)
+        self.assertEquals(mail.outbox[0].from_email, self.newsletter.full_from_email)
 
 
 @override_settings(DISABLE_ENCRYPTION_FOR_TESTS=False)
@@ -387,13 +212,13 @@ class TestHistoricalEvent(MockRequestsTestCase):
         he = HistoricalEvent.objects.all()[0]
         p = Person.objects.all()[0]
         s = Subscription.objects.all()[0]
-        self.assertEquals(he.event_type, "subscribed")
+        self.assertEquals(he.event_type, "transfer-subscription")
         self.assertEquals(he.event_creator_type, "person")
         self.assertEquals(he.event_creator_pk, p.pk)
         self.assertHistoricalEventDataEquality(
             he,
             person=p,
-            event_type="subscribed",
+            event_type="transfer-subscription",
             newsletter=self.newsletter,
             subscription=s,
         )
