6f6a23ea9ebe2d8ac261d4fd8adee81bebbb745f
Steven Skoczen <steven@inkandfeet.com>
2020-10-05 15:15:20 +1300

    Updates to 3.6.12, script cleanup.

--- bin/sync_with_inkshop_repo.sh
+++ bin/sync_with_inkshop_repo.sh
@@ -1,2 +1,10 @@
 #! /bin/sh
-rsync -ac --progress ../inkandfeet-inkshop/ ../inkshop --exclude .git  --exclude .pyc --exclude .DS_Store
+while true; do
+    read -p "Have you committed all changes in inkshop?  Ready for everything to be overwritten? " yn
+    case $yn in
+        [Yy]* ) rsync -ac --progress ../inkandfeet-inkshop/ ../inkshop --exclude .git  --exclude .pyc --exclude .DS_Store; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
